e52e82d2ad7feb1d99cbb67943cb62267a5fc202
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-13 00:49:20 +0400

    updated the messages controller

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
 import authRoutes from './routes/auth.routes.js';
+import userRoutes from './routes/user.routes.js';
+import messageRoutes from './routes/message.routes.js';
+
 import connectToMongoDB from './db/connectToMongoDB.js';
-import messageRoutes from './routes/message.route.js';
+
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -18,6 +22,7 @@ app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
