74903b1d2a250a1d2a75b57bd6892f6fa14ccfc3
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-13 07:38:52 +0400

    added the messages container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css';
-
+import Home from './pages/home/home.jsx';
 import SignUp from './pages/signup/signup.jsx';
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <SignUp />
+      <Home />
     </div>
   );
 }
