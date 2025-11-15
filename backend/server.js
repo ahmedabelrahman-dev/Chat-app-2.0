332eb36153563c661ab4332c5b86b60705000edd
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-15 21:52:10 +0400

    feat: implemented socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+
 import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.routes.js';
@@ -8,15 +8,12 @@ import messageRoutes from './routes/message.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 import cookieParser from 'cookie-parser';
+import { app, server} from './socket/socket.js';
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT;
-app.get('/', (req, res) => {
-  // root route "localhost:5000"
-  res.send('My Chat App 2.0 !!');
-});
+
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
@@ -24,7 +21,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`server is running on port ${PORT}`);
 });
